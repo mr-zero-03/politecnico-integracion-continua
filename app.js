2a029d232c43f6f3ebd24000f5390f1206d4ca0c
mr-zero-03 <danielsantiagoperez03@gmail.com>
2024-11-04 18:10:12 -0500

    Using mysql2 in app.js and using env vars to connect

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const mysql = require('mysql2');
 const app = express();
 const port = 3000;
 
+// MySQL Connection (it uses environment variables from the Docker Compose file)
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+// Connecting to the database
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+    return;
+  }
+  console.log('Connected to MySQL');
+});
+
+// Index
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Hello World! Working! This is for Continuous Integration at the Politécnico Grancolombiano');
 });
 
 app.listen(port, () => {
